e378119456cb40a2bf0054c831ec99208f4196df
=zerobear8530032 <=zerobear8530032@gmail.com>
2025-03-02 16:28:10 +0530

    add comments

--- src/TryHashMaps/LRUCache.java
+++ src/TryHashMaps/LRUCache.java
@@ -4,17 +4,22 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 class LRUCache {
+//	here we use linked hash map so that the order of insertion if preserved
     static LinkedHashMap<Integer, Integer> map;
    static int capacity;
 
     public  LRUCache(int cap) {
         map= new LinkedHashMap();
-        this.capacity=cap;
+        this.capacity=cap;// inital cap
     }
     
     public static int get(int key) {
-    	 if(map.containsKey(key)){
-             int val = map.get(key);
+//    	this is a edge case where when use a simple linked hash set it only store insertion 
+//    	order not operations order means the order remain static once we insert no change in order
+//    	we can manage then when we use get operation we remove the element from the hashset and 
+//    	reinsert it 
+    	 if(map.containsKey(key)){// check value exists
+             int val = map.get(key);// here
              map.remove(key);
              map.put(key,val);
              return val;
@@ -25,15 +30,16 @@ class LRUCache {
     
     public static void put(int key, int value) {
 
-        if (map.containsKey(key)) {
+        if (map.containsKey(key)) {// check the current key avaible then put
             map.remove(key);
             map.put(key, value);
         } else {
-            if (map.size() == capacity) {
-            	int firstkey=map.keySet().iterator().next();
-            	map.remove(firstkey);
+            if (map.size() == capacity) {// apply remove when only capacity reach max
+//            	here insertion order remains means the first key is inserted is always at first index 
+            	int firstkey=map.keySet().iterator().next();// get first key using iterator
+            	map.remove(firstkey);// remove first key
             }
-            map.put(key, value);
+            map.put(key, value);// last put the current values
         }
 
     }
@@ -43,10 +49,3 @@ class LRUCache {
     	System.out.println(lRUCache.get(2));
     	}
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
