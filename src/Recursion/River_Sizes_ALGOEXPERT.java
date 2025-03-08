398cb6800aaa7eddbdd345debec2bc69df165dc8
=zerobear8530032 <=zerobear8530032@gmail.com>
2025-03-08 15:29:49 +0530

    dfs

--- src/Recursion/River_Sizes_ALGOEXPERT.java
+++ src/Recursion/River_Sizes_ALGOEXPERT.java
@@ -35,11 +35,18 @@ package Recursion;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class River_Sizes_ALGOEXPERT {
+//	approch : use iterate over matrix and check for island cells if found we try to find all island cells of that island 
+//	and add it in the ans list
+//	we can use DFS/BFS to find all island cell
+//	finally once all island cell are counted return the output as a list 
+//	time complexity :O(M*N)
+//	space complexity :O(M*N)+ recursion depth 
 	
-	 public static List<Integer> riverSizes(int[][] matrix) {
+	public static List<Integer> riverSizes(int[][] matrix) {
 		 boolean [][] visitedmatrix= new boolean[matrix.length][matrix[0].length];// visited matrix will store all elements 
 		 List<Integer>ans = new ArrayList();// answer list
 		 for(int i =0;i<matrix.length;i++) {// check matrix each element
@@ -52,7 +59,8 @@ public class River_Sizes_ALGOEXPERT {
 		 }
 		 return ans; 
 	 }
-//	 helper will use bfs 
+	 
+//	 helper will use DFS 
 	 public static int helper(int [][] matrix, int row,int col,boolean [][] visited,int count ){
 		 if(row<0 || col<0 || row>=matrix.length || col>=matrix[0].length || visited[row][col] || matrix[row][col]==0) {
 			 return 0;
@@ -77,6 +85,11 @@ public class River_Sizes_ALGOEXPERT {
 		 return count;
 		 
 	 }
+	 public static boolean check (List<Integer> ans,List<Integer> output) {
+		 Collections.sort(ans);
+		 Collections.sort(output);
+		 return ans.equals(output);
+	 }
 	
 	public static void main(String[] args) {
 		//Sample Input
@@ -88,8 +101,17 @@ public class River_Sizes_ALGOEXPERT {
 		  {1, 0, 1, 1, 0},
 		};
 		
-	System.out.println(riverSizes(matrix));
-		
+		List <Integer> output1=new ArrayList(Arrays.asList(1, 2, 2, 2, 5));
+	List<Integer> ans1= riverSizes(matrix);
+	
+	
+	if(check(ans1, output1)) {
+		System.out.println("Case 1 Passed");
+	}else {
+		System.out.println("Case 1 Failed");
+		System.out.println("Expected Ouput :"+ output1);
+		System.out.println("Your Answer :"+ (ans1));
+		}
+	
 	}
-
 }
