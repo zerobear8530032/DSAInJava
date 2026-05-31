592bdd8af79813538a2916c26e02a48acf3b6bac
zerobear <zerobear8530032@gmail.com>
2026-05-31 20:04:46 +0530

    practice

---
+++ src/TwoPointers/CountTheNumberOfSpecialCharactersII_3121.java
@@ -0,0 +1,101 @@
+//3121. Count the Number of Special Characters II
+//Solved
+//Medium
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//You are given a string word. A letter c is called special if it appears both in lowercase and uppercase in word, and every lowercase occurrence of c appears before the first uppercase occurrence of c.
+//
+//Return the number of special letters in word.
+//
+//
+//
+//Example 1:
+//
+//Input: word = "aaAbcBC"
+//
+//Output: 3
+//
+//Explanation:
+//
+//The special characters are 'a', 'b', and 'c'.
+//
+//Example 2:
+//
+//Input: word = "abc"
+//
+//Output: 0
+//
+//Explanation:
+//
+//There are no special characters in word.
+//
+//Example 3:
+//
+//Input: word = "AbBCab"
+//
+//Output: 0
+//
+//Explanation:
+//
+//There are no special characters in word.
+//
+//
+//
+//Constraints:
+//
+//1 <= word.length <= 2 * 105
+//word consists of only lowercase and uppercase English letters.
+package TwoPointers;
+
+import java.util.Arrays;
+
+public class CountTheNumberOfSpecialCharactersII_3121 {
+//    approch :
+//    just compute the first index from left to right when cap letter c comes
+//    and compute the last letter of small letter char comes
+//    then just check if small and capital character present if yes
+//    just check if small last occurence is higher the capital first occurence
+//    Note : make the string index as 1 based indexing will make it handle edge case where thie is only single occurences
+//    time complexity : O(n)
+//    space complexity : O(1)
+
+    public static int numberOfSpecialChars(String word) {
+        int [] small = new int [26];
+        int [] cap = new int [26];
+        Arrays.fill(cap,Integer.MAX_VALUE);
+        for(int i =0;i<word.length();i++){
+            char ch = word.charAt(i);
+            if(ch>='a' && ch<='z'){
+                small[ch-'a']=Math.max(i+1,small[ch-'a']);
+            }else{
+                cap[ch-'A']=Math.min(i+1,cap[ch-'A']);
+            }
+        }
+        int res=0;
+        for(int i =0;i<26;i++){
+            if(small[i]!=0 && cap[i]!=Integer.MAX_VALUE && small[i]<cap[i]){
+                res++;
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        //Example 1:
+
+        String word1 = "aaAbcBC";
+        int output1= 3;
+
+        //Example 2:
+
+        String word2 = "abc";
+        int output2= 0;
+
+        //Example 3:
+
+        String word3 = "AbBCab";
+        int output3= 0;
+
+    }
+}
